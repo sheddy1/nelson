60158995831ca0e1452d31f39b42d5c0f9ddb112
sheddy1 <34214520+sheddy1@users.noreply.github.com>
2021-07-03 04:44:44 -0700

    front end almost complete

--- registration.php
+++ registration.php
@@ -8,8 +8,70 @@
     <link rel="stylesheet" href="index.css">
 </head>
 <body>
-    <div id="registration">
+    <div id="registration"><!-- reg starts -->
+        <form action="registration.php" class="reg-form"><!-- form starts -->
+            <label class="reg-header">
+                *Register here and get your unique 
+                number*
+            </label>
+        
+        <div class="reg-name">
+                <label class="reg-name1">
+                    Name
+                </label>
+                <input type="text" class="reg-name2" placeholder="Type your name here">
 
-    </div>
+            </div>
+
+            <div class="reg-matric">
+            <label class="reg-name1">
+                    Matric Number
+                </label>
+                <input type="text" class="reg-name2" placeholder="Type your name here">
+
+            </div>
+
+            <div class="reg-hall">
+            <label class="reg-name1">
+                    Hall
+                </label>
+                <input type="text" class="reg-name2" placeholder="Type your name here">
+            </div>
+
+            <div class="reg-level">
+            <label class="reg-name1">
+                    Level
+                </label>
+                <input type="text" class="reg-name2" placeholder="Type your name here">
+
+            </div>
+
+            <div class="reg-room">
+            <label class="reg-name1">
+                    Room Number
+                </label>
+                <input type="text" class="reg-name2" placeholder="Type your name here">
+
+            </div>
+
+            <div class="reg-email">
+            <label class="reg-name1">
+                    Email adress
+                </label>
+                <input type="text" class="reg-name2" placeholder="Type your name here">
+            </div>
+
+            <button class="reg-button">
+                REGISTRATION
+            </button>
+        </form>
+
+        <label class="unique1">
+            Your Unique ID is 
+            <label for="">
+
+            </label>
+        </label>
+    </div><!-- reg ends -->
 </body>
 </html>
