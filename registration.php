f8eb1c29667f15ca48808091552ccef6be5d5d23
sheddy1 <34214520+sheddy1@users.noreply.github.com>
2021-07-03 08:42:41 -0700

    reg backend finished

--- registration.php
+++ registration.php
@@ -1,3 +1,13 @@
+<?php
+            $user = "root";
+            $pass = "";
+            $db ="nelson";
+            $db = new mysqli('localhost',$user,$pass,$db) 
+            or die("could not connect");
+
+            
+        ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +19,7 @@
 </head>
 <body>
     <div id="registration"><!-- reg starts -->
-        <form action="registration.php" class="reg-form"><!-- form starts -->
+        <form action="" method="post" class="reg-form"><!-- form starts -->
             <label class="reg-header">
                 *Register here and get your unique 
                 number*
@@ -19,7 +29,7 @@
                 <label class="reg-name1">
                     Name
                 </label>
-                <input type="text" class="reg-name2" placeholder="Type your name here">
+                <input type="text" name="name" class="reg-name2" placeholder="Type your name here">
 
             </div>
 
@@ -27,7 +37,7 @@
             <label class="reg-name1">
                     Matric Number
                 </label>
-                <input type="text" class="reg-name2" placeholder="Type your name here">
+                <input type="text" name="matric" class="reg-name2" placeholder="Type your matric no here">
 
             </div>
 
@@ -35,14 +45,86 @@
             <label class="reg-name1">
                     Hall
                 </label>
-                <input type="text" class="reg-name2" placeholder="Type your name here">
+
+                <select name="hall" class="reg-name2">
+                    <option value="none ">
+                        hall
+                    </option>
+
+                    <option value="peter">
+                        Peter
+                    </option>
+
+                    <option value="john">
+                        john
+                    </option>
+
+                    <option value="paul">
+                        Paul
+                    </option>
+
+                    <option value="joseph">
+                        Joseph
+                    </option>
+
+                    <option value="daniel">
+                        Peter
+                    </option>
+
+                    <option value="mary">
+                        Mary
+                    </option>
+
+                    <option value="deborah">
+                        Deborah
+                    </option>
+
+                    <option value="dorcas">
+                        Dorcas
+                    </option>
+
+                    <option value="hannah">
+                        Hannah
+                    </option>
+
+                    <option value="peace">
+                        Peace
+                    </option>
+                </select>
             </div>
 
             <div class="reg-level">
             <label class="reg-name1">
                     Level
                 </label>
-                <input type="text" class="reg-name2" placeholder="Type your name here">
+                
+                <select name="level" class="reg-name2">
+                    <option value="none ">
+                        Level
+                    </option>
+
+                    <option value="100">
+                        100
+                    </option>
+
+                    <option value="200">
+                        200
+                    </option>
+
+                    <option value="300">
+                        300
+                    </option>
+
+                    <option value="400">
+                        400
+                    </option>
+
+                    <option value="500">
+                        500
+                    </option>
+
+            
+                </select>
 
             </div>
 
@@ -50,7 +132,7 @@
             <label class="reg-name1">
                     Room Number
                 </label>
-                <input type="text" class="reg-name2" placeholder="Type your name here">
+                <input type="text" name="room" class="reg-name2" placeholder="Type your Room here">
 
             </div>
 
@@ -58,20 +140,78 @@
             <label class="reg-name1">
                     Email adress
                 </label>
-                <input type="text" class="reg-name2" placeholder="Type your name here">
+                <input type="text" name="email" class="reg-name2" placeholder="Type your email here">
             </div>
 
-            <button class="reg-button">
-                REGISTRATION
-            </button>
+            
+            <input type="submit" value="REGISTRATION" class="reg-button">
         </form>
 
-        <label class="unique1">
-            Your Unique ID is 
-            <label for="">
+        <?php
+        if(isset($_POST['name']) && isset($_POST['email'])   && isset($_POST['matric']) && isset($_POST['hall']) && isset($_POST['level']) && isset($_POST['room']))
+        { 
+           // 
+            $user = "root";
+            $pass = "";
+            $db ="nelson";
+            $db = new mysqli('localhost',$user,$pass,$db)
+             or die("could not connect");
 
-            </label>
-        </label>
+             $name = $_POST['name'];
+        $matric = $_POST['matric'];
+        $email = $_POST['email'];
+        $hall = $_POST['hall'];
+        $level = $_POST['level'];
+        $room = $_POST['room'];
+
+        
+        
+        if($name == "" && $email == "" && $matric == "" && $hall == "" && $level == "" && $room == "")
+        {
+            echo"<script>alert('Please fill in the blanc spaces!!')</script>";
+        }
+        else{
+            
+            $idnum = rand(1000,9999);
+
+            session_start();
+              $_SESSION['id'] = $idnum;
+
+              
+
+            $insert = "INSERT INTO reg
+            (name,matric,hall,level,room,email,id) 
+            VALUES ('$name','$matric','$hall','$level','$room','$email','$idnum')" ;
+              
+              mysqli_query($db,$insert);
+
+              header("Location: main.php");
+
+              //echo"<script>alert('Your unique ID is: $idnum')</script>";
+
+              //header("Location: main.php");
+        }
+    }
+
+        ?>
+
+        <!--<label class="unique1">
+            <?php
+            $id = $_SESSION['id'];
+            ?>
+            Your Unique ID is 
+            
+                <?php 
+                echo"
+                <label >
+                $id
+
+                </label>
+
+                "
+                ?>
+            
+        </label>-->
     </div><!-- reg ends -->
 </body>
 </html>
